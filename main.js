2f424b24aff0680979b54b460ab17b182afe779d
Krzysztof Jerzyk <krzysztof.jerzyk1993@gmail.com>
2023-10-29 11:17:19 +0100

    correct script tag

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log('To jest plik podstrona załadaowana z osobnego pliku, nie pisany bezpośrednio w html')
+console.log('To jest plik podstrona załadaowana z osobnego pliku, nie pisany bezpośrednio w html');

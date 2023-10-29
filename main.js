a9ad7ecd621880680e10f9e51d53d9fbc8884632
Krzysztof Jerzyk <krzysztof.jerzyk1993@gmail.com>
2023-10-29 11:20:26 +0100

    add js code ane done exercise from modul 3

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('To jest plik podstrona załadaowana z osobnego pliku, nie pisany bezpośrednio w html');
+const age = 30
+const firsnName = 'Krzystzof'
+
+console.log(`Cześć, mama na imie ${firsnName}, mam ${age}lat`)

dfb0bd027e1a3f603b97c98ed691847380e401ea
Krzysztof Jerzyk <krzysztof.jerzyk1993@gmail.com>
2023-10-30 21:59:38 +0100

    end modul 4 excercise

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const age = 30
 const firsnName = 'Krzystzof'
 
 console.log(`Cześć, mama na imie ${firsnName}, mam ${age}lat`)
+
+let zmienna = document.querySelector('.main-header--js')
+zmienna.innerHTML = 'jestem tekstem wstrzyknietym z js'

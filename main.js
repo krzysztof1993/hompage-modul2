5c9ec042f304a1b65d0f847be98a44a01b43580e
Krzysztof Jerzyk <krzysztof.jerzyk1993@gmail.com>
2023-11-02 16:53:56 +0100

    aff function i main.js

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log(`Cześć, mama na imie ${firsnName}, mam ${age}lat`)
 
 let zmienna = document.querySelector('.main-header--js')
 zmienna.innerHTML = 'jestem tekstem wstrzyknietym z js'
+
+const powitanie = (name, age) =>{
+    return `Siema ${name}, serio masz ${age}lat, stara dupa z ciebie`
+}
+
+console.log(powitanie('Krzysztof', 30))
